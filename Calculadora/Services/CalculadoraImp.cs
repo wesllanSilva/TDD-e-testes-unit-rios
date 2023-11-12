141e87f36ec0e66cbb685a513ff80c7c430f568f
Weslan Silva <wesllan90@gmail.com>
2023-11-11 21:53:06 -0300

    add Metodos CalculadoraImp

--- Calculadora/Services/CalculadoraImp.cs
+++ Calculadora/Services/CalculadoraImp.cs
@@ -7,6 +7,27 @@ namespace Calculadora.Services
 {
     public class CalculadoraImp
     {
-        
+        public CalculadoraImp()
+        {
+        }
+        public int Somar(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+
+        public int Subtrair(int valor1, int valor2)
+        {
+            return valor1 - valor2;
+        }
+
+        public int Multiplicar(int valor1, int valor2)
+        {
+            return valor1 * valor2;
+        }
+
+        public int Dividir(int valor1, int valor2)
+        {
+            return valor1 / valor2;
+        }
     }
 }
